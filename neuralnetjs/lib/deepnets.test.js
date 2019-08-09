2cd628a558c6439cfc244bcd5954c9910e00ded5
azselez.scezeznyk <36665025+azselez-scezeznik@users.noreply.github.com>
2019-08-09 16:51:12 +0530

    Add files via upload

---
+++ neuralnetjs/lib/deepnets.test.js
@@ -0,0 +1,4 @@
+var model = new NeuralNetwork();
+model.layer({'type':'input', 'units':5});
+model.layer({'type':'dense', 'units':10, 'activation':sigmoid});
+model.layer({'type':'dense', 'units':2, 'activation':sigmoid});
